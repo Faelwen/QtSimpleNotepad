e48bf6462ddfedb47cf3a7535f7943abf9d304e2
Faelwen <francis.john.mccarthy@googlemail.com>
2017-11-06 22:44:50 +0100

    Add easy functions

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -41,17 +41,17 @@ void MainWindow::on_actionExit_triggered()
 
 void MainWindow::on_actionCut_triggered()
 {
-
+    ui->textEdit->cut();
 }
 
 void MainWindow::on_actionCopy_triggered()
 {
-
+    ui->textEdit->copy();
 }
 
 void MainWindow::on_actionPaste_triggered()
 {
-
+    ui->textEdit->paste();
 }
 
 void MainWindow::on_actionUndo_triggered()
